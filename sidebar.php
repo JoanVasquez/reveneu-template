6170e1015e7bc70f0a7432d4361bacf56e0cd43e
Joan Vasquez <yoloprogramo22@gmail.com>
2020-04-16 09:19:28 -0400

    initial commit

---
+++ sidebar.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The sidebar containing the main widget area.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package revenue
+ */
+
+
+?>
+
+<aside id="secondary" class="widget-area sidebar">
+	<?php if ( !is_user_logged_in() ) { ?>
+		<div class="promo">
+			<a target="_blank" href="https://www.freshthemes.com/themes/revenue">Upgrade to PRO version</a>
+		</div>
+	<?php } ?>
+	
+	<?php if ( ! is_active_sidebar( 'sidebar-1' ) ) { ?>
+		<div class="widget setup-notice">
+			<p><?php echo __('There is no content here', 'revenue'); ?></p>
+			<p><?php echo __('Please put widgets to the <strong>Sidebar</strong>', 'revenue'); ?></p>
+			<p><a href="<?php echo home_url(); ?>/wp-admin/widgets.php"><?php echo __('Okay, I\'m doing now &raquo;', 'revenue'); ?></a></p>
+		</div>
+	<?php } ?>
+
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</aside><!-- #secondary -->
